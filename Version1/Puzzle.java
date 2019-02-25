040fe4fe9de633fba43f01146e5ac0506dbbdcd7
Edu <edu.dado99@gmail.com>
2019-02-25 12:40:12 +0000

    Changes

---
+++ Version1/Puzzle.java
@@ -0,0 +1,244 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.HashMap;
+
+public class Puzzle{
+    public static int[] temp_array= new int[16];
+    public static long start_time, end_time;
+    public static String solution_str = "";
+
+    //------------------Checkers
+    public static boolean isGoal(String table){return solution_str.equals(table);}
+    public static boolean isSolvable(int[] table, int row){
+        int inversions = 0;
+        boolean bottom_even=!(row%2==0);
+        for(int i=0; i<16; i++){
+            if(table[i]==0) continue;
+            for(int j = i+1; j<16; j++){
+                if(table[j] < table[i] && table[j] != 0)
+                    inversions++;
+            }
+        }return (bottom_even) == (inversions %2 == 0);
+    }
+    public static boolean isValid(int r,int c){return (r<4 && r>=0) && (c<4 && c>=0);}
+    //Makers
+    public static String putPath(String s,int p){
+        switch(p){
+            case 0:  s+="PosInicial- "; break;
+            case 1:  s+="Up "; break;
+            case -1:  s+="Down "; break;
+            case 2:  s+="Left "; break;
+            case -2:  s+="Right "; break;
+        }return s;
+    }
+    public static String[] makePath(String moves,String steps){
+        //[0]-sequence of moves
+        //[1]- number of steps taken
+        String[] path = new String[2];
+        path[0]=moves;
+        path[1]=Integer.parseInt(steps)+1+"";
+        return path;
+    }
+    public static String makeArray(int[][] tb){
+        String t="";
+        int k=0;
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                temp_array[k]=tb[i][j];
+                t+=temp_array[k];
+                k++;
+            }
+        }return t;
+    }
+    //Maker Table General---
+    public static Table stepToTake(Table t, int move,HashMap<String,String[]> h, int method,int n){
+        //2-DFS
+        int r=t.getBlankRow(),c=t.getBlankCol();
+        int[][] temp_table=t.getTable();
+        switch(move){
+            case 1: //Move Blank Up (r-1)(c==)
+                if(!isValid(r-1,c)) return null;
+                temp_table[r][c]=temp_table[r-1][c];
+                temp_table[r-1][c]=0;
+                r--;
+            break;
+            case -1: //Move Blank Down (r+1)(c==)
+                if(!isValid(r+1,c)) return null;
+                temp_table[r][c]=temp_table[r+1][c];
+                temp_table[r+1][c]=0;
+                r++;
+            break;
+            case 2: //Move Blank Left (r==)(c-1)
+                if(!isValid(r,c-1)) return null;
+                temp_table[r][c]=temp_table[r][c-1];
+                temp_table[r][c-1]=0;
+            break;
+            case -2: //Move Blank Right (r==)(c+1)
+                if(!isValid(r,c+1)) return null;
+                temp_table[r][c]=temp_table[r][c+1];
+                temp_table[r][c+1]=0;
+            break;
+        }
+        String ts = makeArray(temp_table);
+        if(isGoal(ts)){
+            end_time = System.nanoTime();
+            ts=putPath(h.get(t.getString())[0],move); //makes the final set of moves
+            solutionFound(t.getString(),h,ts);
+        }
+        if(h.containsKey(t)){
+            if(method != 2)
+                return null;
+            int j = Integer.parseInt(h.get(t.getString())[1]);
+            if(n<j){
+                h.remove(t.getString());
+                Table tb = new Table(temp_array,move,ts);
+                return tb;
+            }
+            return null;
+        }
+        Table tb = new Table(temp_array,move,ts); //new node created
+        temp_table=null;
+        return tb;
+    }
+    //Search Methods
+    public static void tableDFS(Table tb){
+        HashMap<String,String[]> h = new HashMap<>(); //Marker for visited elements
+        HashMap<String,String> in_stack = new HashMap<>(); //Marker for nodes in Stack
+        Stack<Table> stack = new Stack<>();
+        Table t;
+        String ts="PosInicial- ";
+        h.put(tb.getString(),makePath(ts,"-1"));
+        int j=12, tmp=0;
+        stack.push(tb);
+        in_stack.put(tb.getString(),null);
+        while(!stack.isEmpty()){
+            t=stack.pop();
+            in_stack.remove(t.getString()); //visited and in Stack
+            /*if(tmp>j){
+                continue;
+            }*/
+            tmp++;
+            for(int i= -2; i<3; i++){
+                if(i == -1*t.getLastMove() || i==0) continue; //Simetric movement, not legal
+                tb= stepToTake(t,i,h,2,tmp);
+                if(!(tb == null) && !in_stack.containsKey(tb.getString())){
+                    //if null, is invalid movement, or not solvable or already existing
+                    ts=putPath(h.get(t.getString())[0], tb.getLastMove());
+                    h.put(tb.getString(),makePath(ts,h.get(t.getString())[1]));
+                    if(h.get(t.getString())[1].equals("79")) continue;
+                    in_stack.put(tb.getString(),null);
+                    stack.push(tb);
+                    tb=null;
+                }
+            }
+
+        }
+        System.out.println("Solução não encontrada:");
+        System.out.println("Número de nós: "+h.size());
+        double sec = (double)(System.nanoTime()-start_time)/ 1_000_000_000.0;
+        System.out.println("Tempo: "+sec+" segundos");
+        return;
+    }
+
+    public static void tableBFS(Table initial){
+        HashMap<String,String[]> h = new HashMap<>(); //Marker
+        Queue<Table> q = new LinkedList<>();
+        Table t,son;
+        String ts=putPath("",initial.getLastMove());
+        h.put(initial.getString(),makePath(ts,"-1"));
+        q.add(initial);
+        while(!q.isEmpty()){
+            t=q.remove();
+            for(int i= -2; i<3; i++){
+                if(i == -1*t.getLastMove() || i==0) continue; //Simetric movement, not legal
+                son= stepToTake(t,i,h,0,0);
+                if(!(son == null)){
+                    //if null, is invalid movement, or not solvable or already existing
+                    ts=putPath(h.get(t.getString())[0], son.getLastMove());
+                    h.put(son.getString(),makePath(ts,h.get(t.getString())[1]));
+                    q.add(son);
+                }
+            }
+        }
+        System.out.println("Solução não encontrada:");
+        System.out.println("Número de nós: "+h.size());
+        double sec = (double)(System.nanoTime()-start_time)/ 1_000_000_000.0;
+        System.out.println("Tempo: "+sec+" segundos");
+        return;
+    }
+    //----------------------------------
+
+    //----------------------------------
+    public static void solutionFound(String parent,HashMap<String,String[]> h,String moves){
+        int steps = Integer.parseInt(h.get(parent)[1]) +1;
+        long total_time= end_time - start_time;
+        double seconds = (double)total_time/ 1_000_000_000.0;
+        System.out.println("Solução encontrada:");
+        System.out.println("Número de movimentos: "+steps);
+        System.out.println("Movimentos:\n "+moves);
+        System.out.println("Número de nós criados: "+h.size());
+        System.out.println("Tempo total: "+seconds+" segundos");
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] f = new int[16];
+        String str = "";
+        int tmp, blank_row=0,br_f=0;
+
+        //Scan Initial State Table
+            int k = 0;
+            for(int i = 0; i<4; i++){
+                for(int j = 0; j<4; j++){
+                    tmp = in.nextInt();
+                    temp_array[k] = tmp;
+                    str += tmp;
+                    if(tmp == 0) blank_row = i;
+                    k++;
+                }
+            }
+        //Scan Final State Table
+            k=0;
+            for(int i = 0; i<16; i++){
+                tmp = in.nextInt();
+                solution_str +=tmp;
+                if(tmp == 0) br_f = i;
+                f[k]=tmp;
+                k++;
+            }
+        //Tests
+        start_time = System.nanoTime();
+        if(isGoal(str)){
+            System.out.println("Solução óptima, tabelas iguais");
+            System.exit(0);
+        }
+        if(!(isSolvable(temp_array,blank_row) == isSolvable(f,br_f))){
+            System.out.println("Não tem solução");
+            System.exit(0);
+        }
+
+        System.out.println("Escolha o método:");
+        System.out.print("1)BFS--2)DFS--3)IDFS--4)A*--5)Greedy");
+        Table init = new Table(temp_array,0,str);
+        tmp=in.nextInt(); System.out.println("\n");
+        switch(tmp){
+            case 1:
+                System.out.println("BFS:");
+                start_time=System.nanoTime();
+                tableBFS(init);
+            break;
+            case 2:
+                System.out.println("DFS:");
+                start_time=System.nanoTime();
+                tableDFS(init);
+            break;
+            case 3: break;
+            case 4: break;
+            case 5: break;
+            default: System.out.println("Opção errada :("); break;
+        }
+    }
+}
--- Version1/Table.java
+++ Version1/Table.java
@@ -1,48 +1,77 @@
 class Table{
+    //instead of using a table we use an array, saves more memory;
     private String str_table="";
+    private int row,col; //blank Position
+    private int[] table;
     private int last_move;
         //0 ->initial
         //1 -1 -> Up Down
         //2 -2 -> Left Right
-    private int row,col; //blank Position
-    private int[][] table;
-    //Constructor (options)
-        public Table(int[][] table, int last_move){
-            this.last_move=last_move; //Saves the move used to reach this legal configuration
-            this.table = new int[4][4];
-            for(int i=0;i<4;i++){ //move through rows
-                for(int j=0;j<4;j++){
-                    if(table[i][j]==0){this.row=i;this.col=j;}
-                    this.str_table+=table[i][j];
-                    this.table[i][j]=table[i][j];
-                }
-            }
-        }
+    //Constructor
         public Table(int[] array_table, int last_move, String str_table){
             this.last_move=last_move;
             this.str_table=str_table;
-            this.table=new int[4][4];
+            table = new int[16];
             int i=0; //index for the array positions
             for(int j=0;j<4;j++){
                 for(int k=0;k<4;k++){
                     if(array_table[i]==0){this.row=j;this.col=k;}
-                    this.table[j][k]=array_table[i];
+                    table[i]=array_table[i];
                     i++;
                 }
             }
         }
-
     //Getters
         public int getBlankRow(){return this.row;}
         public int getBlankCol(){return this.col;}
         public int getLastMove(){return this.last_move;}
+        public String getString(){return this.str_table;}
         public int[][] getTable(){ //returns a coppy table
             int[][] tmp=new int[4][4];
+            int k=0;
             for(int i=0;i<4;i++){
-                for(int j=0;j<4;j++)
-                    tmp[i][j]=this.table[i][j];
+                for(int j=0;j<4;j++){
+                    tmp[i][j]=table[k];
+                    k++;
+                }
             }
             return tmp;
-        }
-        public String getString(){return this.str_table;}
+        }/*
+        //instead of using a table we use an array, saves more memory;
+        private String str_table="";
+        private int row,col; //blank Position
+        private int last_move;
+            //0 ->initial
+            //1 -1 -> Up Down
+            //2 -2 -> Left Right
+        //Constructor
+            public Table(int last_move, String str_table){
+                this.last_move=last_move;
+                this.str_table=str_table;
+                String[] s = str_table.split(" ");
+                int i=0; //index for the array positions
+                for(int j=0;j<4;j++){
+                    for(int k=0;k<4;k++){
+                        if(s[i].equals("0")){this.row=j;this.col=k;}
+                        i++;
+                    }
+                }
+            }
+        //Getters
+            public int getBlankRow(){return this.row;}
+            public int getBlankCol(){return this.col;}
+            public int getLastMove(){return this.last_move;}
+            public String getString(){return this.str_table;}
+            public int[][] getTable(){ //returns a coppy table
+                int[][] tmp=new int[4][4];
+                String[] s=this.str_table.split(" ");
+                int k=0;
+                for(int i=0;i<4;i++){
+                    for(int j=0;j<4;j++){
+                        tmp[i][j]=Integer.parseInt(s[k]);
+                        k++;
+                    }
+                }
+                return tmp;
+            }*/
 }
