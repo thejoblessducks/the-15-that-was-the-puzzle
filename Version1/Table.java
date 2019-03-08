9230b7d67ee21e129a22bf91ac344e2005e77a94
eamorgado <39220282+eamorgado@users.noreply.github.com>
2019-03-08 23:20:38 +0000

    Possible Final Version

--- Version1/Table.java
+++ Version1/Table.java
@@ -1,6 +1,26 @@
 import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
 class Table{
-    //instead of using a table we use an array, saves more memory;
+    //Class to store a configuration of the board
+    /*
+        Contains 4 values:
+            -> The string configuration of the board (str_table)
+            -> The row of the blank cell in board
+            -> The collum of the blank cell in board
+            -> the matrix representing the board
+            -> The last move (Left->-2/Up->1/Down->-1/Right->2)used to reach this configuration
+        Operations;
+            -> 2 Constructors, one giving a matrix, and one giving an array
+            -> Check if move to be performed is valid
+            -> Get the row where blank cell lies
+            -> Get collum where blank cell lies
+            -> Get the last move to reach this configuration
+            -> Get the string configuration of this board
+            -> Get a copy of this board's matrix
+            -> Get a LinkedList of all the configuration generated from this one using operators
+    */
     private String str_table="";
     private int row,col; //blank Position
     private int[][] table;
@@ -43,8 +63,16 @@ class Table{
         public LinkedList<Table> getDescedents(){
             int r=this.row,c=this.col;
             LinkedList<Table> descedents = new LinkedList<>();
+            //Possible Implementation if we want to randomly generate the moves
+                /*HashSet<Integer> set = new HashSet<>();
+                Random rand = new Random();
+                int j;
+                while(set.size()!=4){
+                    j=rand.nextInt((2-(-2)+1))+(-2);
+                    set.add(j);
+                }*/
             Table t;
-            for(int i=-2;i<3;i++){
+            for(int i=-2;i<3;i++)/*: set)*/{
                 if(i == -1*this.last_move || i==0) continue; //Simetric movement, not legal
                 switch(i){
                     case 1: //Move Blank Up (r-1)(c==)
